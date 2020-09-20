37effda320c5d6b1117b8710f097633ba50e23df
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-09-20 17:32:44 +0200

    factory: add default factory
    helper to fetch the "good" plugin type for the needs

---
+++ factory/factory.go
@@ -0,0 +1,30 @@
+package factory
+
+import (
+	"fmt"
+
+	"github.com/tormath1/plugin"
+	"github.com/tormath1/plugin/local"
+	"github.com/tormath1/plugin/remote"
+)
+
+var (
+	plugins = map[plugin.Type]plugin.Plugin{
+		plugin.Local: local.Local{},
+		plugin.Remote: remote.Remote{},
+	}
+)
+
+// Get returns the specific Plugin for t
+func Get(t string) (plugin.Plugin, error) {
+	ts, err := plugin.TypeString(t)
+	if err != nil {
+		return nil, fmt.Errorf("no plugin type for: %s", ts)
+	}
+
+	p, ok := plugins[ts]
+	if !ok {
+		return nil, fmt.Errorf("no plugin defined for: %s", ts)
+	}
+	return p, nil
+}
