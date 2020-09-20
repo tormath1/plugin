fe03c6061401ac656a96b8fce9ca3f060940461c
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-09-20 17:32:32 +0200

    type: add local, remote and embedded types

---
+++ type.go
@@ -0,0 +1,13 @@
+package plugin
+
+// Type defines the type of the plugin
+type Type int
+
+//go:generate ./../bin/enumer -type=Type -transform=lower -output=type_string.go
+const (
+	Local Type = iota
+	Remote
+	Embedded
+)
+
+
