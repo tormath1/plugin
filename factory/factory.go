fbfcd4c3d2124870714a962b80f1dfb1c2708cc5
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-09-21 11:15:19 +0200

    factory: add embedded type

--- factory/factory.go
+++ factory/factory.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 
 	"github.com/tormath1/plugin"
+	"github.com/tormath1/plugin/embedded"
 	"github.com/tormath1/plugin/local"
 	"github.com/tormath1/plugin/remote"
 )
 
 var (
 	plugins = map[plugin.Type]plugin.Plugin{
-		plugin.Local: local.Local{},
-		plugin.Remote: remote.Remote{},
+		plugin.Local:    local.Local{},
+		plugin.Remote:   remote.Remote{},
+		plugin.Embedded: embedded.Embedded{},
 	}
 )
 
