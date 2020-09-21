98b61b669eab15590b9a4a7a39f3899c9942b7c1
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-09-21 16:08:40 +0200

    type: fix enumer comment

--- type.go
+++ type.go
@@ -3,7 +3,7 @@ package plugin
 // Type defines the type of the plugin
 type Type int
 
-//go:generate ./../bin/enumer -type=Type -transform=lower -output=type_string.go
+//go:generate ./bin/enumer -type=Type -transform=lower -output=type_string.go
 const (
 	Local Type = iota
 	Remote
